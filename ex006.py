ecdf1d1ec8fefa1660be175a14b9a63a347cb70e
Gustavo Moura <gustavoo.moura3@gmail.com>
2021-10-14 01:18:11 -0300

    commit inicial com os exercicios prontos

---
+++ ex006.py
@@ -0,0 +1,3 @@
+# Programa que exibe o dobro, o triplo e a raiz quadrada no numero digitado
+numero = int(input('Digite um número: '))
+print('O dobro é {}, o triplo é {}, e a raíz quadrada é {}'.format(numero * 2, numero * 3, numero ** (1/2)))
